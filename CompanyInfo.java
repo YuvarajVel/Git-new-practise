c9e923f0d59f845cca07111d892cdce8199da43a
YuvarajVel <yuvi95.v2@gmail.com>
2023-07-16 13:26:08 +0530

    practise

---
+++ CompanyInfo.java
@@ -0,0 +1,22 @@
+package org.company;
+
+public class CompanyInfo {
+private void companyName() {
+	// TODO Auto-generated method stub
+System.out.println("company name");
+}
+private void companyId() {
+	// TODO Auto-generated method stub
+System.out.println("company id");
+}
+private void companyAddress() {
+	// TODO Auto-generated method stub
+System.out.println("company address");
+}
+public static void main(String[] args) {
+	CompanyInfo ci=new CompanyInfo();
+	ci.companyAddress();
+	ci.companyId();
+	ci.companyName();
+}
+}
